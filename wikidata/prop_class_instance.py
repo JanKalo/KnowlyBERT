849fdfe859effa810f0a3f94b0323de8013af56a
Leandra Fichtel <fichtel@is68.ifis.cs.tu-bs.de>
2020-03-25 23:51:37 +0100

    worked on prop_classes with instances

--- wikidata/prop_class_instance.py
+++ wikidata/prop_class_instance.py
@@ -15,7 +15,7 @@ def find_class(entity_id_urls):
         #finding instance
         query = """SELECT ?instance 
                 WHERE {{
-                    <{}> <http://www.wikidata.org/prop/direct/P31> ?instance
+                    <http://www.wikidata.org/entity/{}> <http://www.wikidata.org/prop/direct/P31> ?instance
                 }}""".format(url)
         cursor.execute("SPARQL "+query)
         while True:
@@ -43,44 +43,38 @@ def get_most_common_classes(classes):
                 break
     return most_common_classes
 
-def main():
-    file_sentences = open("../LM_sentence/prop_sentence.json", "r")
-    line = file_sentences.readline().split("\n")[0]
-    props = []
-    while line != "":
-        data = json.loads(line)
-        for prop in data:
-            props.append(prop)
-        line = file_sentences.readline().split("\n")[0]
-    file_sentences.close()
+def read_config_file():
+    #parsing the config file
+    config_file = open("../config.json", "r")
+    dictio_config = json.load(config_file)
+    config_file.close()
+    return dictio_config
 
+def main():
+    #dictio_config = read_config_file()
+    #dataset = open(dictio_config["wikidata_missing_tripel_path"])
+    dataset = open("../P1412_dictio_wikidata_objects.json", "r")
+    dictio_wikidata_objects = json.load(dataset)
+    dataset.close()
+    #props = ['P19', 'P20', 'P279', 'P37', 'P413', 'P166', 'P449', 'P69', 'P47', 'P138', 'P364', 'P54', 'P463', 'P101', 'P1923', 'P106', 'P527', 'P102', 'P530', 'P176', 'P27', 'P407', 'P30', 'P178', 'P1376', 'P131', 'P1412', 'P108', 'P136', 'P17', 'P39', 'P264', 'P276', 'P937', 'P140', 'P1303', 'P127', 'P103', 'P190', 'P1001', 'P31', 'P495', 'P159', 'P36', 'P740', 'P361']
+    props = ["P1412"]
     dictio_classes = {}
     for p in props:
         try:
-            if p != "P31" and p != "P279":
-                items = []
-                values = []
-                print(p)
-                query = """SELECT DISTINCT ?item ?value
-                        WHERE {{
-                            ?item <http://www.wikidata.org/prop/direct/{}> ?value
-                        }}""".format(p)
-                cursor.execute("SPARQL "+query)
-                while True:
-                    row = cursor.fetchone()
-                    if not row:
-                        break
-                    if row.item not in items:
-                        items.append(row.item)
-                    if row.value not in values:
-                        values.append(row.value)
-                item_classes = find_class(items)
-                values_classes = find_class(values)
-                temp = {}
-                temp["?PQ"] = get_most_common_classes(item_classes)
-                temp["QP?"] = get_most_common_classes(values_classes)
-                dictio_classes[p] = temp
-
+            items = []
+            values = []
+            subjects = dictio_wikidata_objects[p]
+            for subj in subjects:
+                items.append(subj)
+                objects = dictio_wikidata_objects[p][subj]["random_incomplete"]
+                for obj in objects:
+                    values.append(obj)
+            item_classes = find_class(items)            
+            values_classes = find_class(values)
+            temp = {}
+            temp["?PQ"] = get_most_common_classes(item_classes)
+            temp["QP?"] = get_most_common_classes(values_classes)
+            dictio_classes[p] = temp
         except Exception as e:
             print(p)
             print(e)
